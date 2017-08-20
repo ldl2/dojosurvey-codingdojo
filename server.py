b382be25f7ae50d796ce89361b122dd644093563
ldl2 <ericdrakejones@gmail.com>
2017-08-20 10:08:08 -0500

    done

--- server.py
+++ server.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = "sosecret"
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/result', methods=['POST'])
+@app.route('/preresult', methods=['POST'])
 def button():
-    name = request.form["name"]
-    location = request.form["location"]
-    language = request.form["language"]
-    comment = request.form["comment"]
-    print(name)
-    return render_template('/result.html', name=name, location=location, language=language, comment=comment)
+    session['name'] = request.form["name"]
+    session['location'] = request.form["location"]
+    session['language'] = request.form["language"]
+    session['comment'] = request.form["comment"]
+    if len(request.form['name']) > 0:
+        flash("Success! Your name is {}".format(request.form['name']))
+    else:
+        flash("Name cannot be empty!")
+        return redirect('/')
+    if len(request.form['comment']) > 1:
+        if len(request.form['comment']) < 120:
+            flash("Success! Your comment is {}".format(request.form['comment']))
+        else:
+            flash("Too many characters in comment!")
+            return redirect('/')
+    else:
+        print('fuck')
+        flash("Comment cannot be empty")
+        return redirect('/')
+    return redirect('/result')
+
+@app.route('/result')
+
+def realresult():
+    return render_template('/result.html')
+
 app.run(debug=True)
